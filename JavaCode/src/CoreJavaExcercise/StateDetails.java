b1482dbac56db6eb320c814f02775e420f6832b8
garghimani <58023554+garghimani@users.noreply.github.com>
2019-11-27 16:47:20 -0800

    Final Commit.

--- JavaCode/src/CoreJavaExcercise/StateDetails.java
+++ JavaCode/src/CoreJavaExcercise/StateDetails.java
@@ -11,21 +11,21 @@ public class StateDetails {
 				"Connecticut", "Delaware", "Florida", "Georgia");
 		List<String> state_capital = Arrays.asList("Montgomery", "Juneau", "Phoenix", "Little Rock", "Sacramento",
 				"Denver", "Hartford", "Dover", "Tallahassee", "Atlanta");
-		
+
 		System.out.println("CODE   STATE NAME      CAPITAL");
 		System.out.println("----   ----------      --------");
-		
+
 		Iterator<String> itr_code = state_code.listIterator();
 		Iterator<String> itr_name = state_name.listIterator();
 		Iterator<String> itr_capital = state_capital.listIterator();
-		
-		while(itr_code.hasNext()||itr_name.hasNext()||itr_capital.hasNext()) {
-			
-			System.out.print(itr_code.next()+"      "+itr_name.next()+"       "+itr_capital.next());
+
+		while (itr_code.hasNext() || itr_name.hasNext() || itr_capital.hasNext()) {
+
+			System.out.print(itr_code.next() + "      " + itr_name.next() + "       " + itr_capital.next());
 			System.out.println();
-			
+
 		}
-		
+
 	}
 
 }
